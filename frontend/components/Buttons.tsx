55a5878b51dfc238488b271f15cdc6d0d028078f
Alexander <klapka91@protonmail.com>
2022-07-20 08:51:53 +0200

    some buttons

---
+++ frontend/components/Buttons.tsx
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  padding: 5px 10px;
+  font-size: ${({ theme }) => theme.typeScale.smallParagraph};
+  border-radius: 2px;
+  /* min-width: 100px; */
+  cursor: pointer;
+  font-family: ${({ theme }) => theme.font.primary};
+  font-weight: 300;
+`;
+
+export const PrimaryButton = styled(Button)`
+  background-color: ${({ theme }) => theme.colors.primary};
+  color: black;
+  border: 2px solid transparent;
+
+  :hover {
+    background-color: white;
+  }
+`;
+
+export const SecondaryButton = styled(Button)`
+  background-color: white;
+  border: 3px solid ${({ theme }) => theme.colors.primary};
+  color: ${({ theme }) => theme.colors.primary};
+  border-radius: 0;
+`;
+
+export const TertiaryButton = styled(Button)`
+  background-color: white;
+  border: 2px solid transparent;
+  color: ${({ theme }) => theme.colors.primary};
+`;
